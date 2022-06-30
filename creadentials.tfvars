bd48038f1df95c045a8452f1149effe7def78d6b
Gopal <gopalthakur71@gmail.com>
2022-06-30 17:17:20 +0530

    2nd commit

---
+++ creadentials.tfvars
@@ -0,0 +1,3 @@
+
+access_key = AKIAVCJVFULBQLFTSX7Y
+secret_key = 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
--- providers.tf
+++ providers.tf
@@ -8,3 +8,9 @@ terraform {
 
   required_version = ">= 1.2.0"
 }
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  version    = "~> 4.16"
+}
